29b61cd52171a3a632e3da24af5cba1e1a2a27a0
Chuck Lantz <clantz@microsoft.com>
2015-05-29 19:42:42 -0700

    Update remove-android.cmd

--- tips-and-workarounds/android/security-05-26-2015/remove-android.cmd
+++ tips-and-workarounds/android/security-05-26-2015/remove-android.cmd
@@ -4,4 +4,4 @@ REM Copyright (c) Microsoft. All rights reserved.
 REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 call npm install -g cordova
-call cordova platform remove android --save
+call cordova platform remove android
